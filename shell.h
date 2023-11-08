23d682e844a0e6236499fbf3dffa6aa0b618776f
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-11-08 14:56:13 -0800

    Header file updated.

--- shell.h
+++ shell.h
@@ -1,6 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#define BUFFER_SIZE 128
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,5 +10,7 @@
 #include <sys/wait.h>
 
 int _strlen(char *str);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
+ssize_t _getline(char **inputPTR, size_t *size, int input_STREAM);
+int handle_failure(char *ptr);
 #endif
