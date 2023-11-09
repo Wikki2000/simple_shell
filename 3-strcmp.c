dae403739b5a306f5be8b61581d76128ceb86616
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-11-09 12:20:26 -0800

    String Compare Function.

--- 3-strcmp.c
+++ 3-strcmp.c
@@ -8,8 +8,16 @@
  *
  * Return: Value (0) if success else (1)
  */
-int stringCOMPARE(const char *str1, const char *str2, int index)
+int stringCOMPARE(char *str1, char *str2, int index)
 {
+	int str_LEN1 = 0, str_LEN2 = 0;
+
+	if (str1 == NULL || str2 == NULL)
+		return (-1);
+	str_LEN1 = _strlen(str1);
+	str_LEN2 = _strlen(str2);
+	if (str_LEN1 != str_LEN2)
+		return (1);
 	if (str1[index] == '\0' && str2[index] == '\0')
 		return (0);
 
