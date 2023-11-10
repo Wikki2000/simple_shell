a0d5191a51dd936d21cd48e73de0a0d61919ca38
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-11-09 16:32:36 -0800

    Main function updated.

--- 4-shell.c
+++ 4-shell.c
@@ -1,53 +1,68 @@
 #include "shell.h"
 
 /**
+ *  str_tokenize - tokenize a command accepted from user
+ * @input: user input to be tokenized
+ * @args: buffer to store the tokenized string
+ *
+ * Return: array of tokenized user input
+ */
+
+char **str_TOKENIZE(char *input, char **args)
+{
+	char *token = NULL;
+	int ac = 0;
+
+	token = strtok(input, " ");
+	while (token != NULL)
+	{
+		args[ac] = token;
+		ac++;
+		token = strtok(NULL, " ");
+	}
+	args[ac] = NULL;
+	return (args);
+}
+
+/**
  * main - Entry point of program
  * @argc: Command-line arguement count
- * @args: An array of command-line arguement
+ * @argv: An array of command-line arguement
  * @envp: Environment variable
  *
  * Description: This program implement a simple shell
  *
  * Return: Always (0)
  */
-int main(int argc, char **args, char **envp)
+
+int main(int argc, char **argv, char **envp)
 {
-	(void)args;
+	char *input = NULL, **args = NULL;
+	size_t size = 0;
+	int val;
+
 	(void)argc;
+	(void)argv;
 
-	for (;;)
+	while (1)
 	{
-		int i;
-		char *input = getINPUT();
-		char *token;
-		char **command;
-
+		val = getINPUT(&input, &size);
+		if (val == -1)
+			break;
 		if (_strlen(input) == 0)
-		{
-			free(input);
 			continue;
-		}
-
 		if (stringCOMPARE(input, "exit", 0) == 0)
 			exit(EXIT_SUCCESS);
-
-		command = (char **) malloc(sizeof(char *) * 100);
-		if (command == NULL)
+		args = (char **)malloc(sizeof(char *) * 100);
+		if (args == NULL)
 		{
 			perror("Memory allocation fail");
 			exit(EXIT_FAILURE);
 		}
-
-		token  = _strtok(input, " ");
-		command[0] = token;
-		for (i = 1; token != NULL; i++)
-		{
-			token = _strtok(NULL, " ");
-			command[i] = token;
-		}
-		command[i] = NULL;
-		executeCOMMAND(command, envp);
-		free(input);
+		str_TOKENIZE(input, args);
+		executeCOMMAND(args, envp);
+		free(args);
 	}
+	free(input);
 	return (0);
 }
