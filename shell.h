d7614cf7422e1a9852e536e9ae77587d0ccb23d9
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-11-09 16:33:57 -0800

    Header file updated.

--- shell.h
+++ shell.h
@@ -15,10 +15,11 @@ int _strlen(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
 ssize_t _getline(char **inputPTR, size_t *size, int input_STREAM);
 int handle_failure(char *ptr);
-char *getINPUT();
+int getINPUT(char **buff, size_t *size);
 char *_strchr(const char *str, int c);
 char *_strtok(char *str, const char *delim);
 void executeCOMMAND(char **args, char **envp);
 int stringCOMPARE(char *str1, char *str2, int index);
+char **str_TOKENIZE(char *input, char **args);
 
 #endif
