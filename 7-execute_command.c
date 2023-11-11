c0d025c305758af0c44d0f2e529af1c0319ba990
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-11-11 13:46:44 -0800

    Execute command function.

--- 7-execute_command.c
+++ 7-execute_command.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * executeCOMMAND - Execute a command in a child process
  * @args: Array of strings representing the command and its arguments
@@ -11,90 +12,20 @@
  */
 void executeCOMMAND(char **args, char **envp)
 {
-	char *paths = getenv("PATH"), *path = NULL, command[100];
+	char command[100];
 	pid_t babyPROCCESS;
 	int isError = 1;
 
-	path = _strtok(paths, ":");
-	while (path != NULL)
-	{
-		_strcpy(command, path);
-		if (args[0][0] == '/')
-			_strcpy(command, args[0]);
-		else
-			_strcat(command, args[0]);
-		if (access(command, X_OK) == 0)
-		{
-			babyPROCCESS = fork();
-			if (babyPROCCESS == -1)
-			{
-				perror("Fork failed");
-				exit(EXIT_FAILURE);
-			}
-			else if (babyPROCCESS == 0)
-			{
-				execve(command, args, envp);
-				perror("./hsh");
-				exit(EXIT_FAILURE);
-			}
-			else
-			{
-				isError = 0;
-				wait(NULL);
-				break;
-			}
-		}
-		path = _strtok(NULL, ":");
-	}
-	if (isError)
-		perror("./hsh");
-}
-/**
- * _getenv - get path form the environmetal variable
- * @env_NAME: name of environmental variable
- *
- * Return: the value of the environmntal variable
- *
- * Description: This function extract the value of the external variable
- */
-char *_getenv(char *env_NAME)
-{
-	int i, len;
-	char *val;
-
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		val = _strchr(environ[i], '=');
-		if (val != NULL)
-		{
-			len = val - environ[i];
-			if (stringCOMPARE(environ[i], env_NAME, len) == 0 && env_NAME[len] == '\0')
-			{
-				return ((char *)(val + 1));
-			}
-		}
-	}
-	return (NULL);
-	pid_t babyPROCCESS = fork();
-	char *command;
-
-	command = (char *) malloc(sizeof(char));
-	if (command == NULL)
-	{
-		perror("Malloc fail");
-		exit(EXIT_FAILURE);
-	}
-
-	if (_strncmp(args[0], "/bin/", 5) == 0)
-		strcpy(command, args[0]);
+	if (args[0][0] == '/')
+		_strcpy(command, args[0]);
 	else
 	{
-		strcpy(command, "/bin/");
-		strcat(command, args[0]);
+		_strcpy(command, "/bin/");
+		_strcat(command, args[0]);
 	}
-
 	if (access(command, X_OK) == 0)
 	{
+		babyPROCCESS = fork();
 		if (babyPROCCESS == -1)
 		{
 			perror("Fork failed");
@@ -103,11 +34,15 @@ char *_getenv(char *env_NAME)
 		else if (babyPROCCESS == 0)
 		{
 			execve(command, args, envp);
-			exit(EXIT_SUCCESS);
+			perror("./hsh");
+			exit(EXIT_FAILURE);
 		}
 		else
+		{
+			isError = 0;
 			wait(NULL);
+		}
 	}
-	else
+	if (isError)
 		perror("./hsh");
 }
