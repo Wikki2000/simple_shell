f7d053c65d17c134ad7dc4aaf64f4b6e780c0ade
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-11-11 07:44:04 -0800

    Function to concatenate two strings.

---
+++ strcat.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return:the concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = _strlen(dest), j, i = _strlen(src);
+
+	*(dest + len) = '/';
+	len += 1;
+	for (j = 0; j < i; ++j)
+	{
+		*(dest + len) = *(src + j);
+		len++;
+	}
+	*(dest + len) = '\0';
+	return (dest);
+}
