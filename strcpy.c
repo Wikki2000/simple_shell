48f2b59110a1437fe07c4bc2a40f9056bdf3dab9
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-11-11 07:45:01 -0800

    Function to copy a string.

---
+++ strcpy.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+/**
+ * _strcpy - copy a string into another
+ * @dest: a strring to have a copy
+ * @src: a string to be copied
+ * Return: a copy of string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, len = strlen(src);
+
+	for (i = 0; i < len; ++i)
+	{
+		*(dest + i) = *(src + i);
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
