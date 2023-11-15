7402d0bae40755da3388350040a63e776467554a
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-11-15 07:51:06 -0800

    Header file.

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#define BUFFER_SIZE 128
+#define MAX_COMMAND_LENGTH 64
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+int _strlen(const char *string);
+#endif
